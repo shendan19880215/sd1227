00dd87e2561dc3464f51a8087f9a710769800977
shendan19880215 <100544871+shendan19880215@users.noreply.github.com>
2022-08-19 18:11:02 +0900

    Update sd1227.sh

--- sd1227.sh
+++ sd1227.sh
@@ -1,1 +1,12 @@
-mkdir azagent;cd azagent;curl -fkSL -o vstsagent.tar.gz https://vstsagentpackage.azureedge.net/agent/2.206.1/vsts-agent-linux-x64-2.206.1.tar.gz;tar -zxvf vstsagent.tar.gz; if [ -x "$(command -v systemctl)" ]; then ./config.sh --environment --environmentname "test1227env" --acceptteeeula --agent $HOSTNAME --url https://dev.azure.com/v-danshen/ --work _work --projectname 'test1227' --auth PAT --token 4462epe5ch7w2e3h4kj3yfpvn5czfar6ifm2jtxex666guypqosq --runasservice; sudo ./svc.sh install; sudo ./svc.sh start; else ./config.sh --environment --environmentname "test1227env" --acceptteeeula --agent $HOSTNAME --url https://dev.azure.com/v-danshen/ --work _work --projectname 'test1227' --auth PAT --token 4462epe5ch7w2e3h4kj3yfpvn5czfar6ifm2jtxex666guypqosq; ./run.sh; fi
+#!/bin/bash
+mkdir azagent
+cd azagent
+curl -fkSL -o vstsagent.tar.gz https://vstsagentpackage.azureedge.net/agent/2.206.1/vsts-agent-linux-x64-2.206.1.tar.gz
+tar -zxvf vstsagent.tar.gz
+if [ -x "$(command -v systemctl)" ]
+then ./config.sh --environment --environmentname "test1227env" --acceptteeeula --agent $HOSTNAME --url https://dev.azure.com/v-danshen/ --work _work --projectname 'test1227' --auth PAT --token 4462epe5ch7w2e3h4kj3yfpvn5czfar6ifm2jtxex666guypqosq --runasservice
+sudo ./svc.sh install
+sudo ./svc.sh start
+else ./config.sh --environment --environmentname "test1227env" --acceptteeeula --agent $HOSTNAME --url https://dev.azure.com/v-danshen/ --work _work --projectname 'test1227' --auth PAT --token 4462epe5ch7w2e3h4kj3yfpvn5czfar6ifm2jtxex666guypqosq
+./run.sh
+fi
